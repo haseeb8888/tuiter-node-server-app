39c76c2296f8d66c6da8f61231c4e2d96d2c1fd4
Abdul Haseeb Hussain Mohammed <haseeb98h@gmail.com>
2022-11-18 15:29:30 -0500

    updated createTuit response

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,11 +1,29 @@
 import posts from "./tuits.js";
 let tuits = posts;
 
+const currentUser = {
+  "userName": "NASA",
+  "handle": "@nasa",
+  "image": "http://www.nasa.gov/sites/default/files/images/nasaLogo-570x450.png",
+};
+
+const templateTuit = {
+  ...currentUser,
+  "topic": "Space",
+  "time": "2h",
+  "liked": false,
+  "replies": 0,
+  "retuits": 0,
+  "likes": 0,
+  "dislikes": 0
+}
+
 const createTuit = (req, res) => {
-    const newTuit = req.body;
+  const newTuit = {
+    ...req.body,
+    ...templateTuit
+};
     newTuit._id = (new Date()).getTime()+'';
-    newTuit.likes = 0;
-    newTuit.liked = false;
     tuits.push(newTuit);
     res.json(newTuit);
   }
